b8423e0f981b4351fb87a42f7dcef9f0daef9912
sanzhardelltop <yandexsbot@gmail.com>
2026-03-20 12:20:19 +0500

    Fix Flask binding with PORT environment variable and host 0.0.0.0

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from flask import Flask, request
-from threading import Thread
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 5000))
+
 @app.route('/')
-def home():
-    return "VK Bot is running"
-
-def run_flask():
-    app.run(port=5000)
-
-def run_vk_bot():
-    vk_session = vk_api.VkApi(token='YOUR_TOKEN_HERE')
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            print(f'New message: {event.text}')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    Thread(target=run_flask).start()
-    run_vk_bot()
+    app.run(host='0.0.0.0', port=port)
