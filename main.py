98e3596afb8e15b8d97a08b6d3e0e6dd930aae69
sanzhardelltop <yandexsbot@gmail.com>
2026-03-20 12:33:50 +0500

    Updated main.py to use PORT environment variable and bind Flask to 0.0.0.0

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# Bind to 0.0.0.0 with environment variables for VK bot
-VK_TOKEN = os.getenv('VK_TOKEN')
-VK_GROUP_ID = os.getenv('VK_GROUP_ID')
-
-@app.route('/')
-def hello():
-    return 'Hello, VK!'
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
